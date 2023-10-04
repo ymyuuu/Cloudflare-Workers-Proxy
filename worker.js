ea0715747ec4c0eb47a4b56e369d852f2c286111
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2023-10-04 07:20:27 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,20 +1,31 @@
+// 监听 'fetch' 事件，当有请求到达时执行回调函数
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+// 处理传入的请求
 async function handleRequest(request) {
+  // 解析请求的 URL
   const url = new URL(request.url);
 
-  // 检查路径是否以 /https:// 开头
-  if (!url.pathname.startsWith('/https://')) {
-    const mainDomain = new URL(request.url).origin;
-    return new Response(`请使用正确的地址格式: ${mainDomain}/https://yourdomain.com`, { status: 400 });
-
-  }
-
-  // 提取目标地址
+  // 从路径中提取目标 URL，解码特殊字符
   const targetUrl = decodeURIComponent(url.pathname.slice(1))
 
+  // 如果没有提供目标 URL，则返回错误响应
+  if (!targetUrl) {
+    const mainDomain = url.hostname;
+
+    const errorMessage = `
+      请使用正确的地址格式:
+      https://${mainDomain}/https://yourdomain.com<br>
+      例如: <a href="https://${mainDomain}/https://github.com/ymyuuu" target="_blank">https://${mainDomain}/https://github.com/ymyuuu</a>
+    `;
+
+    // 返回带有错误消息的响应和状态码 400（错误的请求）
+    return new Response(errorMessage, { status: 400, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
+  }
+
+  // 创建一个新的请求，以便访问目标 URL
   const modifiedRequest = new Request(targetUrl, {
     headers: request.headers,
     method: request.method,
@@ -23,29 +34,19 @@ async function handleRequest(request) {
   });
 
   try {
+    // 发起对目标 URL 的请求
     const response = await fetch(modifiedRequest);
     
-    // 处理不同的HTTP状态代码
-    if (response.status === 200) {
-      // 如果目标地址返回200 OK，将其返回给客户端
-      const modifiedResponse = new Response(response.body, response);
-      modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
-      return modifiedResponse;
-    } else if (response.status === 404) {
-      // 如果目标地址返回404 Not Found，返回自定义错误消息
-      return new Response('目标地址不存在', { status: 404 });
-    } else if (response.status === 500) {
-      // 如果目标地址返回500 Internal Server Error，返回自定义错误消息
-      return new Response('目标地址出现内部服务器错误', { status: 500 });
-    } else if (response.status === 522) {
-      // 如果目标地址返回522 Origin Connection Time-out，返回自定义错误消息
-      return new Response('目标地址连接超时', { status: 522 });
-    } else {
-      // 处理其他状态代码，可以根据需要添加更多条件
-      return new Response('目标地址返回未知状态: ' + response.status, { status: response.status });
-    }
+    // 创建一个新的响应，使用目标 URL 的响应内容和状态
+    const modifiedResponse = new Response(response.body, response);
+    
+    // 设置允许跨域访问的响应头
+    modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+    
+    // 返回修改后的响应
+    return modifiedResponse;
   } catch (error) {
-    // 处理fetch失败的情况
+    // 如果请求目标地址时出现错误，返回带有错误消息的响应和状态码 500（服务器错误）
     return new Response('无法访问目标地址: ' + error.message, { status: 500 });
   }
 }
